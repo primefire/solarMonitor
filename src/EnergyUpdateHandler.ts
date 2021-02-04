0599e5c411755fad2c4a13037b8c40034d671997
Lennart <northman@outlook.de>
2021-02-04 10:45:32 +0100

    replaced cron

--- src/EnergyUpdateHandler.ts
+++ src/EnergyUpdateHandler.ts
@@ -1,51 +1,49 @@
-import cron from 'node-cron';
 import EnergyReadingModel from './Database/Models/EnergyReadingModel';
 
 import TotalEnergyReading from './Models/TotalEnergyReading';
 
 export default class EnergyUpdateHandler {
-	private latestReading!: TotalEnergyReading;
+	private latestTimestampString!: string;
 
 	constructor() {
 		this.bindFunctions();
-		this.scheduleCronJobForEveryHour();
 	}
 
 	private bindFunctions(): void {
 		this.handleEnergyUpdate = this.handleEnergyUpdate.bind(this);
-		this.saveLatestReadingWithCleanTimestampInDatabaseIfAvailable = this.saveLatestReadingWithCleanTimestampInDatabaseIfAvailable.bind(this);
 		this.getCurrentHourTimestamp = this.getCurrentHourTimestamp.bind(this);
+		this.doesThisTimestampStartANewHour = this.doesThisTimestampStartANewHour.bind(this);
+		this.saveLatestReadingWithCleanTimestampInDatabase = this.saveLatestReadingWithCleanTimestampInDatabase.bind(this);
 	}
 
-	private scheduleCronJobForEveryHour(): void {
-		cron.schedule('* * * * *', this.saveLatestReadingWithCleanTimestampInDatabaseIfAvailable);
-	}
-
-	private saveLatestReadingWithCleanTimestampInDatabaseIfAvailable(): void {
-		console.log('trying');
-		if (this.latestReading) {
-			let reading = this.latestReading;
-			reading.timestamp = this.getCurrentHourTimestamp();
-			let databaseModel = new EnergyReadingModel({
-				timestamp: reading.timestamp,
-				produced: reading.energyProduced,
-				imported: reading.energyImported,
-				exported: reading.energyExported
-			});
-			databaseModel.save();
-			console.log('saved');
+	public handleEnergyUpdate(totalEnergyReading: TotalEnergyReading): void {
+		let timestampString = this.getCurrentHourTimestamp().toISOString();
+		if (this.doesThisTimestampStartANewHour(timestampString)) {
+			this.saveLatestReadingWithCleanTimestampInDatabase(totalEnergyReading);
 		}
+		this.latestTimestampString = timestampString;
 	}
 
 	private getCurrentHourTimestamp(): Date {
 		let timestamp = new Date();
 		timestamp.setUTCMilliseconds(0);
 		timestamp.setUTCSeconds(0);
-		//timestamp.setUTCMinutes(0);
+		timestamp.setUTCMinutes(0);
 		return timestamp;
 	}
 
-	public handleEnergyUpdate(totalEnergyReading: TotalEnergyReading): void {
-		this.latestReading = totalEnergyReading;
+	private doesThisTimestampStartANewHour(timestamp: string): boolean {
+		return this.latestTimestampString != null && this.latestTimestampString !== timestamp;
+	}
+
+	private saveLatestReadingWithCleanTimestampInDatabase(reading: TotalEnergyReading): void {
+		reading.timestamp = this.getCurrentHourTimestamp();
+		let databaseModel = new EnergyReadingModel({
+			timestamp: reading.timestamp,
+			produced: reading.energyProduced,
+			imported: reading.energyImported,
+			exported: reading.energyExported
+		});
+		databaseModel.save();
 	}
 }
